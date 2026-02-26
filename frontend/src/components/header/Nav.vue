c2ee707e4f9772af7052e55c17fd6157c4accbc3
Levitre <levitre.mathys30@gmail.com>
2026-02-26 13:15:03 +0100

    feat(UI): add main nav

---
+++ frontend/src/components/header/Nav.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+const navigations = [
+    'Home',
+    'How to use',
+    'Profile'
+]
+
+</script>
+
+<template>
+
+<nav>
+    <ul>
+        <li v-for="nav in navigations" :key="nav"> {{ nav }}</li>
+    </ul>
+</nav>
+
+</template>
+
+<style>
+
+nav ul {
+    display: flex;
+    list-style: none;
+    gap: 10px;
+}
+
+</style>
